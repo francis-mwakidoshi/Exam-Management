8571b1bad7c50bedce6fe35286525bf99e77b5e5
francis-mwakidoshi <francismwakidoshi@gmail.com>
2022-08-28 19:25:39 +0300

    implemeting update logics

--- mysqlConnect.php
+++ mysqlConnect.php
@@ -1,4 +1,4 @@
 <?php
 
-$con=mysqli_connect("localhost","root","root123","sarah") OR die('connection error. Contact System Owner');
+$con=mysqli_connect("localhost","root","","sarah") OR die('connection error. Contact System Owner');
  ?>
